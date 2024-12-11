1f4fb173e9e0f3f274be9e7f33b5b31eb08980ff
adasmi <asmigaj05@gmail.com>
2024-12-11 11:45:37 +0100

    Automatic board setup and console board visualizer

--- client/src/main/java/org/example/Board.java
+++ client/src/main/java/org/example/Board.java
@@ -4,25 +4,34 @@ public class Board {
 
     private BoardVisualizer bv = new ConsoleBoardVisualizer();
 
-    private int[][] state = {
-            {7,7,7,7,7,7,7,7,7,7,7,7,4,7,7,7,7},
-            {7,7,7,7,7,7,7,7,7,7,7,4,4,7,7,7,7},
-            {7,7,7,7,7,7,7,7,7,7,4,4,4,7,7,7,7},
-            {7,7,7,7,7,7,7,7,7,4,4,4,4,7,7,7,7},
-            {7,7,7,7,3,3,3,3,0,0,0,0,0,5,5,5,5},
-            {7,7,7,7,3,3,3,0,0,0,0,0,0,5,5,5,7},
-            {7,7,7,7,3,3,0,0,0,0,0,0,0,5,5,7,7},
-            {7,7,7,7,3,0,0,0,0,0,0,0,0,5,7,7,7},
-            {7,7,7,7,0,0,0,0,0,0,0,0,0,7,7,7,7},
-            {7,7,7,2,0,0,0,0,0,0,0,0,6,7,7,7,7},
-            {7,7,2,2,0,0,0,0,0,0,0,6,6,7,7,7,7},
-            {7,2,2,2,0,0,0,0,0,0,6,6,6,7,7,7,7},
-            {2,2,2,2,0,0,0,0,0,6,6,6,6,7,7,7,7},
-            {7,7,7,7,1,1,1,1,7,7,7,7,7,7,7,7,7},
-            {7,7,7,7,1,1,1,7,7,7,7,7,7,7,7,7,7},
-            {7,7,7,7,1,1,7,7,7,7,7,7,7,7,7,7,7},
-            {7,7,7,7,1,7,7,7,7,7,7,7,7,7,7,7,7},
-    };
+    Board() {
+        initBoard(5);
+    }
+
+    private int[][] state;
+
+    private void initBoard(int n) {
+        int size = 4*n-3;
+        state = new int[size][size];
+        for (int i=0; i<size; i++) {
+            for (int j=0; j<size; j++) {
+                if (i<n-1) {
+                    if (j <= size - n && j >= size - n - i) state[i][j] = 4;
+                } else if (i<2*n-2) {
+                    if (j <= size - n && j >= size - n - i) state[i][j] = 7;
+                    else if (j > size - n && j < size - i + n - 1) state[i][j] = 5;
+                    else if (j>=n-1 && j < size-n) state[i][j] = 3;
+                } else if (i == size/2) {
+                    if (j >= n-1 && j <= size - n) state[i][j] = 7;
+                } else {
+                    if (state[size-i-1][size-j-1] == 4) state[i][j] = 1;
+                    else if (state[size-i-1][size-j-1] == 3) state[i][j] = 6;
+                    else if (state[size-i-1][size-j-1] == 5) state[i][j] = 2;
+                    else state[i][j] = state[size-i-1][size-j-1];
+                }
+            }
+        }
+    }
 
     public int[][] getState() {
         return state;
@@ -30,7 +39,7 @@ public class Board {
 
     public void move(int y1, int x1, int y2, int x2) {
         state[y2][x2] = state[y1][x1];
-        state[y1][x1] = 0;
+        state[y1][x1] = 7;
     }
 
     public void print() {
--- client/src/main/java/org/example/ConsoleBoardVisualizer.java
+++ client/src/main/java/org/example/ConsoleBoardVisualizer.java
@@ -5,6 +5,16 @@ import java.util.Arrays;
 public class ConsoleBoardVisualizer implements BoardVisualizer{
     public void showBoard(Board board) {
         // TODO ladne wyswietlenie planszy w konsoli, na razie jest tylko wydrukowanie tablicy intów
-        System.out.println(Arrays.deepToString(board.getState()));
+        int[][] s = board.getState();
+        int size = s.length;
+        for (int i = 0; i< size; i++) {
+            for (int j = 0; j< i; j++) System.out.print(" ");
+            for (int j = 0; j < size; j++) {
+                if (s[i][j] == 0) System.out.print("  ");
+                else if (s[i][j] == 7) System.out.print("o ");
+                else System.out.print(s[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
