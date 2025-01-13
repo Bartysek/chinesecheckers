71dc1bc85b1d728caa5bf05c4917f10cace58423
Bartysek <bartosz.mlodzinski04@gmail.com>
2025-01-13 19:29:17 +0100

    implemented standard rules to server

--- server/src/main/java/org/example/NaturalRules.java
+++ server/src/main/java/org/example/NaturalRules.java
@@ -5,20 +5,28 @@ package org.example;
 public class NaturalRules implements RulesInterface {
     private Board board;
     boolean isFirstMoveInTurn = true;
+    int[] pieceOwnership = new int[6];
+    int winner;
+
 
     @Override
-    public void setBoard(Board newBoard) {
+    public void setBoard(Board newBoard, int numPlayers) {
         this.board = newBoard;
+        if (numPlayers != 4) {
+            for (int i = 0; i < 6; i++) {
+                pieceOwnership[i] = i % numPlayers;
+            }
+        }
     }
 
     @Override
-    public int handleMove(int y1, int x1, int y2, int x2) {
-        int status = checkMove(y1, x1, y2, x2);
+    public int handleMove(int y1, int x1, int y2, int x2, int playerNumber) {
+        int status = checkMove(y1, x1, y2, x2, playerNumber);
         if (status == -1) { return status; }
-        else if (status == 0) {
+        else if (status == 1) {
             isFirstMoveInTurn = true;
         }
-        else if (status == 1) {
+        else if (status == 0) {
             isFirstMoveInTurn = false;
         }
         board.move(y1, x1, y2, x2);
@@ -26,21 +34,26 @@ public class NaturalRules implements RulesInterface {
         return status;
     }
 
-    private int checkMove(int y1, int x1, int y2, int x2) {
+    private int checkMove(int y1, int x1, int y2, int x2, int playerNumber) {
         int sum_distance = Math.abs(x1 - x2) + Math.abs(y1 - y2);
 
-        if (y1 == y2 && x1 == x2) { return 0; }
+        if (y1 == y2 && x1 == x2) {
+            return 1;
+        }
+        if (pieceOwnership[board.getState()[x1][y1]] != playerNumber) {
+            return -1;
+        }
 
-        else if ((sum_distance == 1) || (sum_distance == 2 && x1 - x2 == -(y1 - y2) )) {
+        if ((sum_distance == 1) || (sum_distance == 2 && x1 - x2 == -(y1 - y2) )) {
             if (board.getState()[x2][y2] == 7) {
-                return 0;
+                return 1;
             }
             else return -1;
         }
         else if ((sum_distance == 2 && (x1 == x2 || y1 == y2)) || (sum_distance == 4 && x1 - x2 == -(y1 - y2))) {
             int inBetween = board.getState()[(x1 + x2) / 2][(y1 + y2) / 2];
             if (inBetween < 7 && inBetween > 0) {
-                return 1;
+                return 0;
             }
             else {
                 return -1;
@@ -50,7 +63,40 @@ public class NaturalRules implements RulesInterface {
         return -1;
     }
 
-    private boolean checkWinCon() {
-        return false;
+    public boolean checkEndCon(int PlayerNumber) {
+        int size = board.getHexagonSide();
+        int[] lookupChange = {1, 2, 1, -1, -2, -1}; //getting to the peak of the triangle to go along the left and right edge of it
+        int[] lookupLeftX = {0, -1, -1, 0, 1, 1}; //RightX[i] = LeftX[i+1]; LeftY[i] = -LeftX[i+1]; RightY[i] = -LeftX[i+2]
+        for(int i = 0, x = 3 * size - 3, y = 0;
+            i < 6;
+            x += (size - 1) * lookupChange[(i + 2) % 6], y += (size - 1) * lookupChange[i % 6], i++) {
+            if (pieceOwnership[i] == PlayerNumber) {
+                int topX = x;
+                int topY = y;
+                for (int j = 0; j < size - 1; j++) {
+                    int currentX = topX;
+                    int currentY = topY;
+                    for (int k = 0; k < size - j - 1; k++) {
+                        //System.out.println(currentX + " " + currentY + " = " + board.getState()[currentY][currentX] + " ? " + (i+1));
+                        //what the actual fuck is happening
+                        if (board.getState()[currentY][currentX] != i + 1) {
+                            return false;
+                        }
+                        //go 1 left on the triangle
+                        currentX += lookupLeftX[i];
+                        currentY -= lookupLeftX[(i + 1) % 6];
+                    }
+                    //go 1 right on the triangle
+                    topX += lookupLeftX[(i + 1) % 6];
+                    topY -= lookupLeftX[(i + 2) % 6];
+                }
+            }
+        }
+        winner = PlayerNumber;
+        return true;
+    }
+
+    public int getWinner() {
+        return winner;
     }
 }
---
+++ server/src/test/java/NaturalRulesTest.java
@@ -0,0 +1,70 @@
+import org.example.Board;
+import org.example.NaturalRules;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class NaturalRulesTest {
+    int[][] getSolvedState(int n) {
+        int size = 4 * n - 3;
+        int[][] state = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i < n - 1) {
+                    if (j <= size - n && j >= size - n - i) state[i][j] = 1;
+                } else if (i < 2 * n - 2) {
+                    if (j <= size - n && j >= size - n - i) state[i][j] = 7;
+                    else if (j > size - n && j < size - i + n - 1) state[i][j] = 2;
+                    else if (j >= n-1 && j < size-n) state[i][j] = 6;
+                } else if (i == size/2) {
+                    if (j >= n-1 && j <= size - n) state[i][j] = 7;
+                } else {
+                    if (state[size-i-1][size-j-1] == 1) state[i][j] = 4;
+                    else if (state[size-i-1][size-j-1] == 6) state[i][j] = 3;
+                    else if (state[size-i-1][size-j-1] == 2) state[i][j] = 5;
+                    else state[i][j] = state[size-i-1][size-j-1];
+                }
+            }
+        }
+        return state;
+    }
+
+    @Test
+    public void testEndCon(){
+        NaturalRules tested = new NaturalRules();
+        Board b = new Board();
+        b.setState(getSolvedState(5));
+        tested.setBoard(b, 2);
+        int[][] s = b.getState();
+        int size = s.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(s[i][j]);
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+        assertTrue(tested.checkEndCon(0));
+        assertTrue(tested.checkEndCon(1));
+    }
+
+    @Test
+    public void testNoEnd(){
+        NaturalRules tested = new NaturalRules();
+        Board b = new Board();
+        b.setState(getSolvedState(5));
+        tested.setBoard(b, 2);
+        b.move(10, 3, 7, 7);
+        int[][] s = b.getState();
+        int size = s.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(s[i][j]);
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+        assertFalse(tested.checkEndCon(0));
+    }
+}
